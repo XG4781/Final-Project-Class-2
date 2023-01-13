3d77c2373ae4e80fe2959fdfd844c91b183ec3cf
The Future Sphere <futuresphere@Thes-MacBook-Air.local>
2023-01-13 16:28:17 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './style.css'
 
 function App() {
+  const [input, setInput] = useState('');
+  const [students, setStudents] = useState<string[]>([]);
+  const [activeEditIndex, setActiveEditIndex] = useState<null | number>(null);
+  const [activeEditInput, setActiveEditInput] = useState<null | string>(null);
+
+  const handleInputChange = (event: any) => {
+    setInput(event.target.value)
+  }
+
+  const handleAddStudent = () => {
+    setStudents([...students, input]);
+  }
+
+  const handleStudentDelete = (index: number) => {
+    const nextStudents = [...students]
+    nextStudents.splice(index, 1);
+    setStudents(nextStudents)
+  }
+
+  const handleTriggerEdit = (index: number) => {
+    setActiveEditIndex(index);
+    setActiveEditInput(students[index])
+  }
+
+  const handleEditInputChange =(event:any)=>{
+    setActiveEditInput(event.target.value)
+  }
+
+  const handleConfirmEdit = (index: number) => {
+    const nextStudents = [...students];
+    nextStudents[index] = activeEditInput || '';
+    setStudents(nextStudents)
+    setActiveEditIndex(null);
+    setActiveEditInput(null);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>What is in the input?: {input}</h1>
+      <div className="form-container"> 
+        <input type="text" onChange={handleInputChange} value={input} />
+        <button onClick={handleAddStudent}>Add student</button>
+      </div>
+      <div className="list-container">
+        {
+          students.map((student, i) => (
+            <span className="student-item" key={i}>
+              {
+                activeEditIndex === i ? 
+                <input onChange={handleEditInputChange} value={activeEditInput || ''} /> 
+                : student 
+              }
+              {
+                (activeEditIndex || activeEditIndex === 0) && activeEditInput ? (
+                  <button onClick={() => handleConfirmEdit(i)}>confirm</button>
+                ) : (
+                  <button onClick={() => handleTriggerEdit(i)}>edit</button>
+                )
+              }
+              <button onClick={() => handleStudentDelete(i)}>delete</button>
+            </span>
+          ))
+        }
+      </div>
     </div>
   );
 }
